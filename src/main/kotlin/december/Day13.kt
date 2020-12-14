06213c826244522ae185d731e64d7e8df0d96d1b
Melvin Schop <M.T.Schop@student.tudelft.nl>
2020-12-14 14:14:33 +0100

    Add day 13 and 14

--- src/main/kotlin/december/Day13.kt
+++ src/main/kotlin/december/Day13.kt
@@ -1,11 +1,55 @@
 package december
 
 import AbstractDay
+import java.util.*
+import kotlin.math.ceil
 
 class Day13 : AbstractDay() {
 
-    override fun part1(): String = "Not implemented yet!"
+    private val input = obtainInput()
+    private val earliestTime = input[0].toInt()
+    private val busIDs = input[1].split(",").map { if (it == "x") -1 else it.toInt() }
 
-    override fun part2(): String = "Not implemented yet!"
+    // Part 1: 3035
+    override fun part1(): String {
+        var bestID = 0
+        var bestTime = Int.MAX_VALUE
+
+        for (id in busIDs) {
+            if (id == -1) continue
+            val firstTime = ceil(earliestTime.toDouble() / id).toInt() * id
+            if (firstTime < bestTime) {
+                bestID = id
+                bestTime = firstTime
+            }
+        }
+        return (bestID * (bestTime - earliestTime)).toString()
+    }
+
+    // Part 2: 725169163285238
+    override fun part2(): String {
+        val checkOrder = mutableListOf<Pair<Int, Int>>()
+        for (index in busIDs.indices) {
+            if (busIDs[index] == -1) continue
+            checkOrder.add(Pair(busIDs[index], index))
+        }
+        checkOrder.sortByDescending { it.first }
+
+        var t = ceil(100_000_000_000_000.0 / checkOrder[0].first).toLong() * checkOrder[0].first - checkOrder[0].second
+        var knownIndex = 1
+        search@ while (true) {
+            var v = 1
+            for (i in 0 until knownIndex) v *= checkOrder[i].first
+            t += v
+
+            for (index in knownIndex until checkOrder.size) {
+                if ((t + checkOrder[index].second) % checkOrder[index].first != 0L) continue@search
+                else if (index < 5) knownIndex = index
+            }
+            break@search
+        }
+
+        return t.toString()
+    }
 
 }
--- src/main/kotlin/december/Day14.kt
+++ src/main/kotlin/december/Day14.kt
@@ -4,8 +4,71 @@ import AbstractDay
 
 class Day14 : AbstractDay() {
 
-    override fun part1(): String = "Not implemented yet!"
+    private val input = obtainInput {
+        val line = it.split('=')
+        val type = line[0].trim()
+        val data = line[1].trim()
+        if (type == "mask") {
+            var andMask = 0L
+            var orMask = 0L
+            for (c in data) {
+                andMask = andMask shl 1
+                orMask = orMask shl 1
+                when (c) {
+                    '1' -> {
+                        andMask = andMask or 1
+                        orMask = orMask or 1
+                    }
+                    'X' -> andMask = andMask or 1
+                }
+            }
+            Mask(andMask, orMask, data)
+        } else {
+            val memes = type.split(Regex("[\\[\\]]"))
+            Mem(memes[1].trim().toLong(), data.toLong())
+        }
+    }
 
-    override fun part2(): String = "Not implemented yet!"
+    // Part 1: 17481577045893
+    override fun part1(): String {
+        var andBitmask = 0L // 1 = unchanged, 0 becomes 0
+        var orBitmask = 0L // 1 becomes 1, 0 = unchanged
+        val mem = mutableMapOf<Long, Long>()
+        for (op in input) {
+            if (op is Mask) {
+                andBitmask = op.andMask
+                orBitmask = op.orMask
+            } else if (op is Mem) {
+                val orData = op.data or orBitmask
+                mem[op.address] = orData and andBitmask
+            }
+        }
+        return mem.map { it.value }.sum().toString()
+    }
 
+    // Part 2: 4160009892257
+    override fun part2(): String {
+        var xBitmask = ""
+        val mem = mutableMapOf<Long, Long>()
+        for (op in input) {
+            if (op is Mask) xBitmask = op.xMask
+            else if (op is Mem) memoryVariations(op.address, xBitmask).forEach { mem[it] = op.data }
+        }
+        return mem.map { it.value }.sum().toString()
+    }
+
+    private fun memoryVariations(x: Long, mask: String, index: Int = mask.length - 1, cur: Long = 0): List<Long> {
+        if (index < 0) return listOf(cur)
+        val next = cur shl 1
+        return when (mask[mask.length - 1 - index]) {
+            '0' -> memoryVariations(x, mask, index - 1, next or (x shr index and 1L))
+            '1' -> memoryVariations(x, mask, index - 1, next or 1L)
+            'X' -> listOf(0L, 1L).flatMap { memoryVariations(x, mask, index - 1, next or it) }
+            else -> error("Invalid character")
+        }
+    }
+
+    interface Op
+    class Mem(val address: Long, val data: Long) : Op
+    class Mask(val andMask: Long, val orMask: Long, val xMask: String) : Op
 }
